adda2e06aa8cb48938690ad2a1c4521d14969367
Erika Heidi <erika@erikaheidi.com>
2022-03-24 14:57:51 +0100

    updating sample config

--- config_sample.php
+++ config_sample.php
@@ -5,9 +5,9 @@ return [
      * Librarian main config
      * Values set here will overwrite default configuration from the /config dir.
      *****************************************************************************************/
-    'site_name' => getenv('SITE_NAME') ?: 'Librarian',
+    'site_name' => getenv('SITE_NAME') ?: 'On Linux Systems',
     'site_author' => getenv('SITE_AUTHOR') ?: '@erikaheidi',
-    'site_description' => getenv('SITE_DESC') ?: 'Minimalist file-based CMS in PHP',
+    'site_description' => getenv('SITE_DESC') ?: 'Guides and reviews of hardware / software running on Linux',
     'site_url' => getenv('SITE_URL') ?: 'http://localhost:8000',
     'site_root' => getenv('SITE_ROOT') ?: '/',
     'site_about' => getenv('SITE_ABOUT') ?: '_p/about',
@@ -22,4 +22,5 @@ return [
     'app_debug' => getenv('APP_DEBUG') ?: true,
     'app_testing_url' => getenv('TEST_BASE_URL') ?: 'http://nginx',
     'devto_username' => getenv('DEVTO_USER'),
+    'templates_path' => getenv('THEME') ?: __DIR__ . '/app/Resources/themes/custom',
 ];
